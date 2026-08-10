cffe7f9f746ade4cc65a0b13fd693ca39ddb0daf
beratbesli <beratbesli@users.noreply.github.com>
2026-08-10 16:31:56 +0300

    Fix tomli unused-ignore mypy error in CI

--- beer_network/config.py
+++ beer_network/config.py
@@ -11,7 +11,7 @@ from typing import Any, Final
 if sys.version_info >= (3, 11):
     import tomllib
 else:
-    import tomli as tomllib  # type: ignore[import-not-found]
+    import tomli as tomllib
 
 
 __all__ = ["BeerNetworkConfig", "load_config"]
